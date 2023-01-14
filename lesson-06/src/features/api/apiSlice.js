fa54bcb991bd4175570b5aa5bbf63339d2dc1a80
TrevorNathan <trevoratwijukire@gmail.com>
2023-01-14 20:36:15 +0300

    ADD: CRUD endpoints

--- lesson-06/src/features/api/apiSlice.js
+++ lesson-06/src/features/api/apiSlice.js
@@ -4,10 +4,40 @@ export const apiSlice = createApi({
 	reducerPath: "api",
 	baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3500" }),
 	endpoints: (builder) => ({
+		// GET TODOS
 		getTodos: builder.query({
 			query: () => "/todos",
+		}),
+		// ADD TODO
+		addTodo: builder.mutation({
+			query: (todo) => ({
+				url: "/todos",
+				method: "POST",
+				body: todo,
+			}),
+		}),
+		// UPDATE TODO
+		updateTodo: builder.mutation({
+			query: (todo) => ({
+				url: `/todos/${todo.id}`,
+				method: "PATCH",
+				body: todo,
+			}),
+		}),
+		// DELETE TODO
+		deleteTodo: builder.mutation({
+			query: ({ id }) => ({
+				url: `/todos/${id}`,
+				method: "DELETE",
+				body: id,
+			}),
 		}),
 	}),
 });
 
-export const { useGetTodosQuery } = apiSlice;
+export const {
+	useGetTodosQuery,
+	useAddTodoMutation,
+	useUpdateTodoMutation,
+	useDeleteTodoMutation,
+} = apiSlice;
